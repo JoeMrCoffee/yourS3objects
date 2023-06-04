bccd15a0e8e8703d7e45d39e2adfb8a187c6c171
Joe <45982832+JoeMrCoffee@users.noreply.github.com>
2023-06-04 21:58:17 +0800

    Add Delete Object to the site

--- work/src/index.php
+++ work/src/index.php
@@ -6,7 +6,13 @@
 	
 	echo "<div class='overview'><ul>";
 	foreach ($iterator as $object) {
-		echo "<li><a href='".$hostName."/".$bucketName."/".$object['Key']."'>".$object['Key']."</a></li>" ;
+		$objectKey = $object['Key'];
+		echo "<li><form action='submitFile.php' method='post'><a href='$hostName/$bucketName/$objectKey'>$objectKey</a>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<input type='hidden' name='bucket' value='$bucketName'>
+				<input type='hidden' name='objectToDel' value='$objectKey'>
+				<input type='submit' name='delObject' value='DELETE' onclick='return confirmfunction()'>
+			</form></li>" ;
 	}
 	
 	echo "</ul></div>";
@@ -15,16 +21,16 @@
 		<div class='popup' id='addObjectForm' style='visibility: hidden;'>
 			<img class='closepopup' onclick='addObject()' src='close.png'>
 			<form action='submitFile.php' method='post' enctype='multipart/form-data'>
-			<p>Choose an image to upload: <input type='file' name='postimage'></p>
-			<p><input type='submit'></p>
+			<p>Choose an image to upload: <input type='file' name='postobject'></p>
+			<p><input type='submit' name='addObject'></p>
 			</form>
 		</div>";
-
 	
 
 /* Additional references:
 	- https://www.stackhero.io/en/services/MinIO/documentations/Getting-started/Connect-to-MinIO-from-PHP
 	- https://www.php.net/manual/en/features.file-upload.post-method.php
+	- https://www.linode.com/docs/products/storage/object-storage/guides/aws-sdk-for-php/
 */
 ?>
 </body>
@@ -37,6 +43,8 @@
 		} 
 		else { groupform.style.visibility = 'hidden'; }
 	}
+	
+	function confirmfunction() { return confirm('Do you really want to delete this object?'); 	}
 
 </script>
 </html>
